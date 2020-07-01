4757df15d03215d58f6a3e3de179998290b45030
Mayuresh Bharmoria <mayureshbharmoria@gmail.com>
2020-07-01 16:01:54 +0530

    Create nema23_YKD2305m.ino
    first commit

---
+++ nema23_YKD2305m.ino
@@ -0,0 +1,60 @@
+// defines pins numbers
+
+const int dirPin  = 3;
+
+const int stepPin = 4; 
+
+const int enPin   = 5;
+
+void setup() {  
+
+  // Sets the three pins as Outputs
+
+  pinMode(stepPin,OUTPUT); 
+
+  pinMode(dirPin,OUTPUT);
+
+  pinMode(enPin,OUTPUT);
+
+  digitalWrite(enPin,LOW);
+
+}
+
+void loop() {
+
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+
+  // around 200 pulses for making one full cycle rotation
+
+  for(int i = 0; i < 200; i++) {
+
+    digitalWrite(stepPin,HIGH); 
+
+    delayMicroseconds(500); 
+
+    digitalWrite(stepPin,LOW); 
+
+    delayMicroseconds(500); 
+
+  }
+
+  delay(1000); // One second delay
+
+  digitalWrite(dirPin,LOW); //Changes the rotations direction
+
+  // Makes 400 pulses for making two full cycle rotation
+
+  for(int x = 0; x < 1600; x++) {
+
+    digitalWrite(stepPin,HIGH);
+
+    delayMicroseconds(1000);
+
+    digitalWrite(stepPin,LOW);
+
+    delayMicroseconds(1000);
+
+  }
+
+  delay(1000);
+}//enc of program
